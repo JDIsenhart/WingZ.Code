14087ccb9428da707bc02cc6cb80de8c286132d6
AnaVukojevic <e4anav@gmail.com>
2019-04-20 13:42:48 -0600

    Add files via upload

---
+++ WingZ_Code/resources/js/scripts.js
@@ -0,0 +1,11 @@
+function loadBirdReports(){
+    
+    for(int i = 0; i < 50; i++){
+        var birdID = birdsightings[i].bird_id;
+        var time = birdsightings[i].sight_time;
+        var loc = birdsightings[i].location;
+        var user = birdsightings[i].username;
+        //var rep = user + " saw " + birdID + " near " + loc + " at " + time;
+        
+    }
+}
