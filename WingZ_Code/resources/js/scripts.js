f9f6acdc284ee8e8a1e067a994e1f6a32d74f6dd
Poketrumpeter <gregorio@fefland.com>
2019-04-20 14:35:41 -0600

    Worked on script for filling bird feed

--- WingZ_Code/resources/js/scripts.js
+++ WingZ_Code/resources/js/scripts.js
@@ -1,6 +1,6 @@
 function loadBirdReports(){
     
-    for(int i = 0; i < 50; i++){
+    for(var i = 0; i < 50; i++){
         var birdID = birdsightings[i].bird_id;
         var time = birdsightings[i].sight_time;
         var loc = birdsightings[i].location;
@@ -8,4 +8,15 @@ function loadBirdReports(){
         //var rep = user + " saw " + birdID + " near " + loc + " at " + time;
         
     }
+}
+
+function populateHTML(){
+    console.log("Hello World");
+    
+    for(var i = 0; i < 10; i++){
+        document.getElementById("feed").innerHTML += '<div class = "container">' +
+            '        <p class = "text-center"><font size = "8">John saw a really cool bird near starbucks that you should go check out.</p>' +
+            '    </div>'
+    }
+
 }
