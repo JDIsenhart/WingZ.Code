90d7def272648259d79ab5b6e654c8c5c604cc8a
Palmer Gill <jamespalmergill@gmail.com>
2019-04-24 10:04:52 -0600

    Add files via upload

--- WingZ_Code/app.js
+++ WingZ_Code/app.js
@@ -3,29 +3,29 @@ const app = express();
 const path = require('path');
 const router = express.Router();
 
-router.get('/',function(req,res){
+app.get('/',function(req,res){
   res.sendFile(path.join(__dirname+'/html/signin_page.html'));
   //__dirname : It will resolve to your project folder.
 });
 
-router.get('/registration',function(req,res){
+app.get('/registration',function(req,res){
   res.sendFile(path.join(__dirname+'/html/registration.html'));
 });
 
-router.get('/home',function(req,res){
+app.get('/home',function(req,res){
   res.sendFile(path.join(__dirname+'/html/birdfeed.html'));
 });
 
-router.get('/report',function(req,res){
+app.get('/report',function(req,res){
   res.sendFile(path.join(__dirname+'/html/BirdReporting.html'));
 });
 
-router.get('/facts', function(req,res){
+app.get('/facts', function(req,res){
 	res.sendFile(path.join(__dirname+'/html/birdFacts.html'));
 });
 
 //add the router
-app.use('/', router);
+//app.use('/', router);
 app.listen(process.env.port || 3000);
 
 console.log('Running at Port 3000');
